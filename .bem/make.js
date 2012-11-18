258092816744f60c9b3529578dc8db13ff1384a2
Vladimir Grinenko <i@tadatuta.com>
2012-11-18 03:25:46 +0200

    use bem make to get bem-bl@0.3

--- .bem/make.js
+++ .bem/make.js
@@ -1,3 +1,24 @@
+MAKE.decl('Arch', {
+
+    getLibraries: function() {
+
+        return {
+            'bem-bl': {
+                type: 'git',
+                url: 'git://github.com/bem/bem-bl.git',
+                treeish: '0.3'
+            }
+            // ,
+            // 'bl-core-bemhtml' : {
+            //     type: 'git',
+            //     url: 'git://github.com/bem/bl-core-bemhtml.git'
+            // }
+        };
+
+    }
+
+});
+
 MAKE.decl('BundleNode', {
 
     getTechs: function() {
