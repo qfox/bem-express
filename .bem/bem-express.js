df1b0e75c8f4113bfe86a3cbcf1e299ca9ff1a8f
Navotno <theapplesins@gmail.com>
2012-11-19 02:10:38 +0200

    Hidden non-global functions in the bemDriver

--- .bem/bem-express.js
+++ .bem/bem-express.js
@@ -3,63 +3,77 @@ var BEM = require('bem'),
     VM = require('vm');
 
 
-exports.render = function(str, options, fn) {
+var bemDriver = function() {
 
-    var app = options.app;
+    var getBemhtml = function(str) {
 
-    if ('development' == app.get('env')) {
-        BEM.api.make().then(function() {
-            return exports.getCreateResult(str);
-        }).then(function(data) {
-            fn(false, data);
-        });
-    } else {
-        exports.getCreateResult(str).then(function(data) {
-            fn(false, data);
-        });
-    }
+        var path = str;
+
+        return BEM.util.readFile(path)
+            .then(function(c) {
+                /** @name BEMHTML variable appears after runInThisContext() call */
+                VM.runInThisContext(c, path);
+                return BEMHTML;
+            });
+
+    };
+
+    var getBemjson = function(str) {
+
+        var path = str.replace('bemhtml', 'bemjson');
+        return BEM.util.readFile(path)
+            .then(function(c) {
+                return VM.runInThisContext(c, path);
+            });
+
+    };
+
+    var getHtml = function(bemhtml, bemjson) {
+
+        return Q.all([bemhtml, bemjson])
+            .spread(function(bemhtml, bemjson) {
+                return bemhtml.apply(bemjson);
+            });
+
+    };
+
+    var getCreateResult = function(str) {
+
+        return getHtml(
+            getBemhtml(str),
+            getBemjson(str));
+
+    };
+
+    return {
+
+        render: function(str, options, fn) {
+
+            var app = options.app;
+
+            process.env.ISPROJECT = true;
+
+            if ('development' == app.get('env')) {
+                BEM.api.make().then(function() {
+                    return getCreateResult(str);
+                }).then(function(data) {
+                    fn(false, data);
+                });
+            } else {
+                getCreateResult(str).then(function(data) {
+                    fn(false, data);
+                });
+            }
+
+        }
+
+    };
 
 };
 
 
-exports.getBemhtml = function(str) {
 
-    var path = str;
 
-    return BEM.util.readFile(path)
-        .then(function(c) {
-            /** @name BEMHTML variable appears after runInThisContext() call */
-            VM.runInThisContext(c, path);
-            return BEMHTML;
-        });
 
-};
 
-exports.getBemjson = function(str) {
-
-    var path = str.replace('bemhtml', 'bemjson');
-    return BEM.util.readFile(path)
-        .then(function(c) {
-            return VM.runInThisContext(c, path);
-        });
-
-};
-
-exports.getHtml = function(bemhtml, bemjson) {
-
-    return Q.all([bemhtml, bemjson])
-        .spread(function(bemhtml, bemjson) {
-            return bemhtml.apply(bemjson);
-        });
-
-};
-
-exports.getCreateResult = function(str) {
-
-    return exports.getHtml(
-        exports.getBemhtml(str),
-        exports.getBemjson(str));
-
-};
-
-exports.__express = exports.render;
+exports.__express = bemDriver().render;
