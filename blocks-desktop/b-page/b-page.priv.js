daa4ab76d7352fc235ee4a90af2d933048e96b4f
Vladimir Grinenko <i@tadatuta.com>
2012-11-20 19:37:46 +0200

    b-page.priv.js example

---
+++ blocks-desktop/b-page/b-page.priv.js
@@ -0,0 +1,15 @@
+blocks['b-page'] = function(data) {
+    return {
+        block: 'b-page',
+        content: [
+            {
+                elem: 'q',
+                content: 'qq'
+            },
+            {
+                elem: 'qq',
+                content: 'blabla'
+            }
+        ]
+    }
+}
